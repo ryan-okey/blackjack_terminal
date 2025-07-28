1e105a21369406a8fce5e0be4ed74ca827e98498
Ryan Okey <nayryeko@gmail.com>
2025-07-27 20:24:34 -0400

    Refactored, checked for bugs

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
 const ps = require("prompt-sync");
 const promptUser = ps();
 
-let suits : string[] = ["hearts", "clubs", "spades", "diamonds"];
+let suits : string[] = ["Hearts", "Clubs", "Spades", "Diamonds"];
 
 var isPlaying : boolean = true;
 var isNewGame : boolean = true;
-var roundOver : boolean = false;
+var shouldRoundContinue : boolean = false;
+var isPlayerTurn : boolean = true;
 
 const cardValues : CardValue[] = [
-    {cardName: "two", value: 2},
-    {cardName: "three", value: 3},
-    {cardName: "four", value: 4},
-    {cardName: "five", value: 5},
-    {cardName: "six", value: 6},
-    {cardName: "seven", value: 7},
-    {cardName: "eight", value: 8},
-    {cardName: "nine", value: 9},
-    {cardName: "ten", value: 10},
-    {cardName: "jack", value: 10},
-    {cardName: "queen", value: 10},
-    {cardName: "king", value: 10},
-    {cardName: "ace", value: (11 | 1)},
+    {cardName: "Two", value: 2},
+    {cardName: "Three", value: 3},
+    {cardName: "Four", value: 4},
+    {cardName: "Five", value: 5},
+    {cardName: "Six", value: 6},
+    {cardName: "Seven", value: 7},
+    {cardName: "Eight", value: 8},
+    {cardName: "Nine", value: 9},
+    {cardName: "Ten", value: 10},
+    {cardName: "Jack", value: 10},
+    {cardName: "Queen", value: 10},
+    {cardName: "King", value: 10},
+    {cardName: "Ace", value: (11 | 1)},
 ]
 
 type CardValue = {
@@ -62,7 +63,6 @@ const shuffleDeck = (cards: Card[]) => {
     return shuffledDeck;
 }
 
-
 function dealStartingCards(cards: Card[], player: string) {
     let dealtCards = []
     for(let i = 0; i < 2; i++){
@@ -71,7 +71,7 @@ function dealStartingCards(cards: Card[], player: string) {
         }
        
     }
-    console.log(`The Dealer dealt two cards to ${player}.\n`);
+    console.log(`The Dealer dealt two cards to ${player}.`);
 
     return dealtCards;
 }
@@ -82,14 +82,19 @@ function dealCard(cards: (Card | undefined)[]) {
 
 function shouldPlayAgain (message : string){
     console.log(message);
-    let shouldPlayAgain = promptUser("Would you like to play again?");
+    let shouldPlayAgain = promptUser("Would you like to play again? (y/n)");
     if(shouldPlayAgain?.toLowerCase() === 'y'){
         console.log("starting new game")
         isNewGame = true;
         isPlaying = false;
-        roundOver = true;
+        shouldRoundContinue = false;
+        isPlayerTurn = false;
     }else{
+        console.log("======GAME OVER=======")
         isPlaying = false;
+        shouldRoundContinue = false;
+        isPlayerTurn = false;
+        isNewGame = false;
     }
 }
 
@@ -101,27 +106,27 @@ function sumCards(cards: (Card | undefined)[]){
 }
 
 //need to return tuple (best score, lowest score)
-function optimalPlay(cards: (Card | undefined)[], numberOfAces: number, player: string) : [number, number]{
+function optimalPlay(cards: (Card | undefined)[], numberOfAces: number) : [lowestScore: number, bestScore: number]{
+    console.log(`There are ${numberOfAces} Aces in the deck`);
     if(numberOfAces == 0){
+        console.log(`${numberOfAces}`)
         let bestScore = sumCards(cards);
         return [bestScore,bestScore];
     }
 
-    console.log(`${player} has ${numberOfAces} aces, finding optimal combination of values`);
+    // console.log(`${player} has ${numberOfAces} aces, finding optimal combination of values`);
     
-    let nonAceSum = sumCards(cards.filter(card => card?.cardValue.cardName != "ace"));
+    let nonAceSum = sumCards(cards.filter(card => card?.cardValue.cardName.toLowerCase() != "ace"));
     let bestScore = nonAceSum;
     let lowestScore = bestScore + numberOfAces;
 
-    console.log(`Non-Ace sum: ${nonAceSum}`);
+    // console.log(`Non-Ace sum: ${nonAceSum}`);
 
     if(nonAceSum + numberOfAces == 21){
         bestScore = nonAceSum + numberOfAces;
         return [bestScore, bestScore];
     }
     
-    //let tempScore = nonAceSum;
-    //breaks with two aces only
     for(let i = numberOfAces; i > 0; i--){
         if(bestScore + 11 + (numberOfAces - 1) <= 21){
             bestScore += 11;
@@ -130,82 +135,118 @@ function optimalPlay(cards: (Card | undefined)[], numberOfAces: number, player: 
         }
     }
 
-    console.log(`Best calulated score: ${bestScore}`);
-    console.log(cards);
-    return [bestScore, lowestScore];
+    // console.log(`DEBUG: Best calulated score: ${bestScore}`);
+    // console.log(cards);
+    // console.log("DEBUG (BEST POSSIBLE SCORE)")
+    return [lowestScore, bestScore];
 }
 
 function findNumberOfAces(cards: (Card | undefined)[]): number{
-    return cards.filter(card => card?.cardValue.cardName == "ace").length;
+    return cards.filter(card => card?.cardValue.cardName.toLowerCase() == "ace").length;
 }
-function dealerTurn(dealersCards: (Card | undefined)[], deck: (Card | undefined)[]) : number{
-    
 
-    let [dealersSum, lowestScore] = optimalPlay(dealersCards, findNumberOfAces(dealersCards), "Dealer")
+function dealerTurn(dealersCards: (Card | undefined)[], deck: (Card | undefined)[]) : number{
+    console.clear();
+    console.log("====== DEALER's TURN =======")
+
+    let [lowestScore, bestScore] = optimalPlay(dealersCards, findNumberOfAces(dealersCards))
     
-    while(dealersSum < 17  && deck.length > 0){
+    //House rules:  Dealer must STAY at a 'soft 17', aka an Ace + Six
+    while(bestScore < 17  && deck.length > 0){
         console.log("Dealer hits.")
         dealersCards.push(dealCard(deck));
         console.log(dealersCards);
-        [dealersSum, lowestScore] = optimalPlay(dealersCards, findNumberOfAces(dealersCards), "Dealer");
+        [lowestScore, bestScore] = optimalPlay(dealersCards, findNumberOfAces(dealersCards));
     }
 
-    return dealersSum;
+    return bestScore;
 }
 
-function isNatural(){
+function displayCards(cards: (Card | undefined)[], name: string){
+    console.log(`\n${name}'s Cards:`);
+    cards.forEach(card => console.log(`[${card?.cardValue.cardName} of ${card?.suit}]`));
+}
 
+function playerTurn(playerCards: (Card | undefined)[], deck : (Card | undefined)[],) : number {
+    
+    console.log("\n====== Player's TURN =======")
+    let [lowestScore, highestScore] = highAndLowSums(playerCards);
+    displayCards(playerCards, "Player");
+    checkWinOrBust(highestScore, lowestScore);
+    
+    while(isPlayerTurn){
+        isPlayerTurn = (promptUser(`Your highest sum is ${highestScore}, lowest sum is ${lowestScore} hit again?`)) === 'y'? true : false;
+        if(isPlayerTurn){ 
+            console.log(`Player chose to ${isPlayerTurn ? "hit again!" : "stay!"}`)
+            if(deck.length > 0){
+                playerCards.push(dealCard(deck)) ;
+                [lowestScore, highestScore] = highAndLowSums(playerCards);
+            }else{
+                shouldPlayAgain("Deck is out of cards!")
+            }
+        }else{
+            console.log(`Player chose to ${isPlayerTurn ? "hit again!" : "stay!"}`)
+        }
+        displayCards(playerCards, "Player");
+        checkWinOrBust(highestScore, lowestScore);
+    }
+
+    return highestScore;
+}
+
+function checkWinOrBust(highestScore : number, lowestScore : number){
+    console.log(`Checking Win or Bust.  Highest: ${highestScore}, Lowest: ${lowestScore}`)
+    if(highestScore == 21 || lowestScore == 21){
+        shouldPlayAgain("\nBLACKJACK!");
+    }else if(lowestScore > 21) {
+        shouldPlayAgain(`BUST! Total: ${lowestScore}`);
+    }
+}
+
+function highAndLowSums(cards: (Card | undefined)[]) : [lowestScore: number, highestScore: number]{
+    console.log(`Debug high and low sums:`);
+    console.log(cards);
+    let aceCount = findNumberOfAces(cards);
+    console.log(`there are ${aceCount} aces in the deck.`)
+    let nonAceSum = sumCards(cards.filter(card => card?.cardValue.cardName.toLowerCase() != "ace"));
+    return [aceCount + nonAceSum, (aceCount * 11) + nonAceSum];
 }
 
 while(isNewGame){
     console.clear();
     console.log("======BLACKJACK=======")
     isPlaying = true;
-    roundOver = false;
+    shouldRoundContinue = true;
+    isPlayerTurn = true;
     let deck = makeDeck();
     let shuffledDeck = shuffleDeck(deck);
 
-    let playersCards = dealStartingCards(shuffledDeck, "player");
-    let dealersCards = dealStartingCards(shuffledDeck, "dealer");
-    console.log("Player's Cards:");
-    console.log(playersCards);
-    console.log("Dealer's Cards:");
-    console.log(dealersCards);
+    let playerCards = dealStartingCards(shuffledDeck, "player");
+    let dealerCards = dealStartingCards(shuffledDeck, "dealer");
+ 
+    console.log("\nDealer's Cards:");
+    console.log(`[${dealerCards[0]?.cardValue.cardName} of ${dealerCards[0]?.suit}]`);
 
     while(isPlaying){
 
         isNewGame = false;
         
-        let [playersSum, lowestScore] = optimalPlay(playersCards, findNumberOfAces(playersCards), "Player");
         
-        if(playersSum == 21){
-            shouldPlayAgain("BLACKJACK!");
-        }else if(playersSum > 21) {
-            shouldPlayAgain(`BUST! Total: ${playersSum}`);
-        }else if(!roundOver){
-            let shouldHitAgain = promptUser(`Your highest sum is ${playersSum}, lowest sum is ${lowestScore} hit again?`)
-            console.log(`Player chose to ${shouldHitAgain === 'y' ? "hit again" : "stay"}`)
-            if(shouldHitAgain?.toLowerCase() === 'y'){
-                console.log("Hitting!")
-                if(shuffledDeck.length > 0){
-                    playersCards.push(dealCard(shuffledDeck)) ;
-                    [playersSum, lowestScore] = optimalPlay(playersCards, findNumberOfAces(playersCards), "Player");
-                    console.log(playersCards);
-                }else{
-                    shouldPlayAgain("Deck is out of cards!")
-                }
+        let playerSum = playerTurn(playerCards, shuffledDeck);
+        //Dealer's turn
+        if(shouldRoundContinue){
+            let dealerSum = dealerTurn(dealerCards, shuffledDeck);
+            displayCards(playerCards, "Player");
+            displayCards(dealerCards, "Dealer");
+            
+            if(dealerSum > 21){
+                shouldPlayAgain(`Dealer BUSTED! ${dealerSum}`)
+            }else if(dealerSum > playerSum){
+                shouldPlayAgain(`Dealer wins! Dealer: ${dealerSum} Player: ${playerSum}`)
+            }else if(dealerSum < playerSum){
+                shouldPlayAgain(`Player wins! Dealer: ${dealerSum} Player: ${playerSum}`)
             }else{
-                //Dealer's turn
-                let dealerSum = dealerTurn(dealersCards, shuffledDeck);
-                if(dealerSum > 21){
-                    shouldPlayAgain(`Dealer BUSTED! ${dealerSum}`)
-                }else if(dealerSum > playersSum){
-                    shouldPlayAgain(`Dealer wins! Dealer: ${dealerSum} Player: ${playersSum}`)
-                }else if(dealerSum < playersSum){
-                    shouldPlayAgain(`Player wins! Dealer: ${dealerSum} Player: ${playersSum}`)
-                }else{
-                    shouldPlayAgain(`It's a PUSH! Dealer: ${dealerSum} Player: ${playersSum}`)
-                }
+                shouldPlayAgain(`It's a PUSH! Dealer: ${dealerSum} Player: ${playerSum}`)
             }
         }
     }
